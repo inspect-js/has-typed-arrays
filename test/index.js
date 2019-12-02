232741ffc1b87a6e78c46c6480510032c281affd
Jordan Harband <ljharb@gmail.com>
2019-12-02 11:40:22 -0800

    Initial tests

---
+++ test/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var test = require('tape');
+var some = require('array.prototype.some');
+
+var hasTypedArrays = require('..');
+
+test('export', function (t) {
+	t.equal(typeof hasTypedArrays, 'function', 'export is a function');
+	t.end();
+});
+
+test('Typed Arrays', function (t) {
+	var anyExist = some([
+		'Float32Array',
+		'Float64Array',
+		'Int8Array',
+		'Uint8Array',
+		'Uint8ClampedArray',
+		'Int16Array',
+		'Uint16Array',
+		'Int32Array',
+		'Uint32Array',
+		'BigInt64Array',
+		'BigUint64Array'
+	], function (TA) {
+		return typeof global[TA] === 'function';
+	});
+
+	t.equal(hasTypedArrays(), !!anyExist, 'returns boolean indicating typed array support');
+	t.end();
+});
